d34ef9bd6977cd584c058559a1478be9f24a6497
Wilfredo <wgald001@fiu.edu>
2014-05-15 16:17:45 -0400

    Added echo statement
    Added to the echo statement that Wilfredo likes cheeseburgers because he's
    so fat and he needs to lose weight.

--- godisreal.php
+++ godisreal.php
@@ -1,4 +1,4 @@
 <?php
 	$loser = "Wilfredo Galdamez";
-	echo "$loser " . "is a big fat atheist loser";
+	echo "$loser " . "is a big fat atheist loser" . ". He likes to eat cheeseburgers because he's so fat.";
 ?>
